a52148d57a69a624b8c9cfbcffabd1aeed48b1e7
tensho1026 <tensho1026@icloud.com>
2024-12-04 21:38:57 +0900

    feat: stagecomponent要

--- src/app/components/Stage.tsx
+++ src/app/components/Stage.tsx
@@ -1,3 +1,24 @@
-export const Stage = () => {
-	return <div>Stage</div>;
-};
+// export const Stage = () => {
+// 	return <div>Stage</div>;
+// };
+import Image from "next/image";
+
+// export const Stage = () => {
+//   return (
+//     <div >
+//      <Image src="/stage.png" alt="Stage Image" style={{width:"525px",height:"151px"}}/>
+//     </div>
+//   );
+// };
+export default function Page() {
+  return (
+    <div style={{ width: '525px', height: '151px' }}>
+      <Image
+        src="/stage.png"
+        alt="Stage Image"
+        layout="intrinsic" // アスペクト比を維持
+        style={{ objectFit: 'contain' }} // 必要ならobjectFitで調整
+      />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
-
+import { Stage } from './components/Stage';
 export default function Home() {
   return (
+    <div>
     <h1>hello world</h1>
+    <Stage />
+    </div>
   );
 }
