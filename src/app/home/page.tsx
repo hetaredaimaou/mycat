18ae0e5faafe1e9ecc21199203f9c44d580ad597
mafyyu <morita0126m@gmail.com>
2024-12-06 23:22:09 +0900

    ログイン確認用のpage編集

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,21 @@
+'use client';
+import {signIn,signOut,useSession} from "next-auth/react";
+
 export default function page() {
-	return <div>Home</div>;
+	const {data:session,status} = useSession();
+	if (!session || !session.user) {
+		return <p>You are not signed in</p>;
+	  }
+	return <div>
+		 <button onClick={()=>{signOut()}}>Sign Out</button>
+		 <button onClick={()=>{signIn(undefined,{callbackUrl:'http://localhost:3000/auth'})}}>Sing In</button>
+		 name:{session.user.name ||""} <br />
+          image:
+          <img
+            src={session.user.image || ""}
+            alt="icon"
+            style={{ width: "100px", height: "100px" }}
+          />
+          <br />
+		Home</div>;
 }
