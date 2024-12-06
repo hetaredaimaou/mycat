4b982e30f107c61fba4287d5e873640fbf63f563
mafyyu <morita0126m@gmail.com>
2024-12-06 22:22:17 +0900

    feat:認証ページの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
 import Image from "next/image";
 import { Stage } from './components/Stage';
-export default function Home() {
+import { useEffect } from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { redirect } from 'next/navigation';
+
+export default function page() {
+  const { data: session, status } = useSession()
+
+  useEffect(()=>{
+    if (status === "authenticated"){
+      redirect('/home')
+    }
+    if (status === "unauthenticated"){
+      redirect('/auth')
+    }
+  },[status])
+  
   return (
     <div>
     <Stage />
