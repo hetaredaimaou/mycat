4edf9561828ad79f33f2a23158bc658d88986fdf
tensho1026 <tensho1026@icloud.com>
2024-12-07 14:39:28 +0900

    feat: Buttonコンポーネントを作成しまし

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,17 +1,23 @@
 "use client";
 
 type Props = {
-	onClick: () => void,
-	label: string
-	buttonColor: string,
-}
+  onClick: () => void;
+  label: string;
+  buttonColor: string;
+};
 export const Button = ({ onClick, label, buttonColor }: Props) => {
-	return (
-		<button
-			style={{ backgroundColor: buttonColor, width: 350, height: 100 }}
-			onClick={() => onClick}
-		>
-			{label}
-		</button>
-	)
+  return (
+    <button
+      style={{ backgroundColor: buttonColor, width: "350px", height: "100px",borderRadius:"100px"}}
+      onClick={() => onClick}
+    > <p  style={{
+			fontSize: '50px',
+			fontWeight:"bold",
+			textAlign:"center",
+			color:"white",
+			}}>
+      {label}
+			</p>
+    </button>
+  );
 };
