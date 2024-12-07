13bb2efe43fec0b2e85db84ab74543a27707675c
tensho1026 <tensho1026@icloud.com>
2024-12-07 13:41:36 +0900

    feat:Headerコンポーネントのデザインを作成しました

--- src/app/home/_components/Header.tsx
+++ src/app/home/_components/Header.tsx
@@ -1,3 +1,51 @@
+import Image from "next/image";
+import ValueDisplay from "./ValueDisplay";
+
 export const Header = () => {
-	return <div>Header</div>;
+  return (
+    <div
+      style={{
+        width: "1100px",
+        height: "100px",
+        backgroundColor: "green",
+				borderTopRightRadius: '100px', 
+        borderBottomRightRadius: '100px',
+				position:"relative"
+      }}
+    >
+      <div
+        style={{
+          width: "750px",
+          height: "80px",
+          display: "flex",
+					backgroundColor:"white",
+					borderRadius:"100px",
+					alignItems: "center",
+					justifyContent: "center",
+					textAlign:"center",
+					gap:"20px",
+					position:"absolute",
+					right:"10px",
+					top:"10px"
+
+        }}
+      >
+       <Image
+          src="/level2.png"
+          alt="Level Icon"
+          width={76}  // ここにピクセル単位でサイズを指定
+          height={60} // ここにピクセル単位でサイズを指定
+        />
+        <ValueDisplay amount='9999+' measure='level' />
+        <Image
+          src="/coin.png"
+          alt="Coin Icon"
+          width={80}  // ここにピクセル単位でサイズを指定
+          height={60} // ここにピクセル単位でサイズを指定
+        />
+        <ValueDisplay amount='9999+' measure='coin'/>
+      </div>
+      
+    </div>
+  );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { Stage } from './components/Stage';
 import ValueDisplay from './home/_components/ValueDisplay';
+import { Header } from "./home/_components/Header";
 
 export default function Home() {
   return (
     <div>
     <Stage />
     <ValueDisplay amount="9999+" measure="coin" />
+    <Header/>
     </div>
   );
 }
